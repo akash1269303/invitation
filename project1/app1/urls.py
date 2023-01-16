d286fd41b1c58268866767d0fce01a92ffdb0cc1
akash1269303 <74952159+akash1269303@users.noreply.github.com>
2023-01-16 21:24:41 +0530

    Update urls.py

--- project1/app1/urls.py
+++ project1/app1/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('success/', views.success, name='success')
 ]
