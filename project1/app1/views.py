5daf91b7144893fa374e9662ed79bbc4ec255e3e
akash1269303 <74952159+akash1269303@users.noreply.github.com>
2023-01-16 21:24:16 +0530

    Update views.py

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -3,10 +3,16 @@ from .forms import MyForm
 # Create your views here.
 
 
+
 def home(request):
     form=MyForm(request.POST,request.FILES)
     if request.method=='POST':
         if form.is_valid():
             form.save()
-
+            return redirect('success')                        
     return render(request,"home.html",{"form":form})
+
+
+def success(request):
+    a='AK'
+    return render(request,'success.html',{'context':a})
