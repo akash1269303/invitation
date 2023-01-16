59143b12d1dab6e80a78513f938a4803bc80e0f4
akash1269303 <74952159+akash1269303@users.noreply.github.com>
2023-01-16 21:25:55 +0530

    Update settings.py

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -136,5 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID =
 AWS_SECRET_ACCESS_KEY=
-AWS_STORAGE_BUCKET_NAME='project-bucket-upload'
+AWS_STORAGE_BUCKET_NAME='project-bucket-upload-1'
 AWS_QUERYSTRING_AUTH=False
